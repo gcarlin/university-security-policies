448d8d007bcf6aa0213a81b83e8411ee55679a65
Julian Sabos <artpick@gmail.com>
2018-11-19 15:00:52 +0100

    JSA: Modify the query

--- university-security-policies-core/src/main/java/org/nuxeo/customsecuritypolicy/UniversityPolicy.java
+++ university-security-policies-core/src/main/java/org/nuxeo/customsecuritypolicy/UniversityPolicy.java
@@ -1,18 +1,16 @@
 package org.nuxeo.customsecuritypolicy;
 
-import org.nuxeo.ecm.core.query.sql.NXQL;
-import org.nuxeo.ecm.core.query.sql.model.*;
-import org.nuxeo.ecm.core.query.sql.model.SQLQuery.Transformer;
-
-import java.security.Principal;
-
 import org.nuxeo.ecm.core.api.NuxeoPrincipal;
 import org.nuxeo.ecm.core.api.security.ACP;
 import org.nuxeo.ecm.core.api.security.Access;
 import org.nuxeo.ecm.core.model.Document;
+import org.nuxeo.ecm.core.query.sql.NXQL;
+import org.nuxeo.ecm.core.query.sql.model.*;
+import org.nuxeo.ecm.core.query.sql.model.SQLQuery.Transformer;
 import org.nuxeo.ecm.core.security.AbstractSecurityPolicy;
 import org.nuxeo.ecm.core.security.SecurityPolicy;
 
+import java.security.Principal;
 import java.util.Optional;
 
 /**
@@ -20,74 +18,90 @@ import java.util.Optional;
  */
 public class UniversityPolicy extends AbstractSecurityPolicy implements SecurityPolicy {
 
-	@Override
-	public Access checkPermission(Document doc, ACP mergedAcp, Principal principal, String permission,
-			String[] resolvedPermissions, String[] additionalPrincipals) {
-		NuxeoPrincipal nxPrinc = (NuxeoPrincipal) principal;
-		
-		if ("Invoice".equals(doc.getType().getName())) {
-			Boolean isConfidential = Optional.ofNullable((Boolean) doc.getValue("university:confidential"))
-					.orElse(Boolean.FALSE);
+    @Override
+    public Access checkPermission(Document doc, ACP mergedAcp, Principal principal, String permission,
+                                  String[] resolvedPermissions, String[] additionalPrincipals) {
+        NuxeoPrincipal nxPrinc = (NuxeoPrincipal) principal;
 
-			if (isConfidential && !nxPrinc.isMemberOf("university")) {
-				return Access.DENY;
-			}
-		}
-		return Access.UNKNOWN;
-	}
+        if ("Invoice".equals(doc.getType().getName())) {
+            Boolean isConfidential = Optional.ofNullable((Boolean) doc.getValue("university:confidential"))
+                    .orElse(Boolean.FALSE);
 
-	@Override
-	public boolean isRestrictingPermission(String permission) {
-		// could only restrict Browse permission, or others
-		return true;
-	}
+            if (isConfidential && !nxPrinc.isMemberOf("university")) {
+                return Access.DENY;
+            }
+        }
+        return Access.UNKNOWN;
+    }
 
-	@Override
-	public boolean isExpressibleInQuery(String repositoryName) {
-		return true;
-	}
+    @Override
+    public boolean isRestrictingPermission(String permission) {
+        // could only restrict Browse permission, or others
+        return true;
+    }
 
-	@Override
-	public SQLQuery.Transformer getQueryTransformer(String repositoryName) {
-		return IS_CONFIDENTIAL_TRANSFORMER;
-	}
+    @Override
+    public boolean isExpressibleInQuery(String repositoryName) {
+        return true;
+    }
 
-	public static final Transformer IS_CONFIDENTIAL_TRANSFORMER = new IsConfidentialTransformer();
+    @Override
+    public SQLQuery.Transformer getQueryTransformer(String repositoryName) {
+        return IS_CONFIDENTIAL_TRANSFORMER;
+    }
 
-	/**
-	 * Sample Transformer that adds {@code (ecm:primaryType = 'Invoice' AND university:confidential = true) OR ...} to
-	 * the query.
-	 */
-	public static class IsConfidentialTransformer implements SQLQuery.Transformer {
+    public static final Transformer IS_CONFIDENTIAL_TRANSFORMER = new IsConfidentialTransformer();
 
-		/** {@code university:confidential = true} */
-		public static final Predicate IS_CONFIDENTIAL = new Predicate(new Reference("university:confidential"),
-				Operator.EQ, new IntegerLiteral(1L));
-		/** {@code ecm:primaryType = 'Invoice'} */
-		public static final Predicate IS_INVOICE = new Predicate(new Reference(NXQL.ECM_PRIMARYTYPE),
-				Operator.EQ, new StringLiteral("Invoice"));
+    /**
+     * Sample Transformer that adds {@code (ecm:primaryType = 'Invoice' AND university:confidential = true) OR ...} to
+     * the query.
+     */
+    public static class IsConfidentialTransformer implements SQLQuery.Transformer {
 
-		public static final Predicate INVOICE_AND_CONFIDENTIAL = new Predicate(IS_CONFIDENTIAL, Operator.AND, IS_INVOICE);
-		
-		@Override
-		public SQLQuery transform(Principal principal, SQLQuery query) {
-			NuxeoPrincipal nxPrinc = (NuxeoPrincipal) principal;
+        /**
+         * {@code university:confidential = true}
+         */
+        public static final Predicate IS_CONFIDENTIAL = new Predicate(new Reference("university:confidential"),
+                Operator.EQ, new IntegerLiteral(1L));
+        /**
+         * {@code ecm:primaryType = 'Invoice'}
+         */
+        public static final Predicate IS_INVOICE = new Predicate(new Reference(NXQL.ECM_PRIMARYTYPE),
+                Operator.EQ, new StringLiteral("Invoice"));
 
-			WhereClause where = query.where;
-			Predicate predicate;
-			if (!nxPrinc.isMemberOf("university")) {
-				if (where == null || where.predicate == null) {
-					predicate = INVOICE_AND_CONFIDENTIAL;
-				} else {
-					// adds an ecm:primaryType = 'Invoice' AND university:confidential = true to the WHERE clause
-					predicate = new Predicate(INVOICE_AND_CONFIDENTIAL, Operator.OR, where.predicate);
-				}
-				// return query with updated WHERE clause
-				return new SQLQuery(query.select, query.from, new WhereClause(predicate), query.groupBy, query.having,
-						query.orderBy, query.limit, query.offset);
-			}
-			return query;
+        /**
+         * {@code ecm:primaryType <> 'Invoice'}
+         */
+        public static final Predicate IS_NOT_INVOICE = new Predicate(new Reference(NXQL.ECM_PRIMARYTYPE),
+                Operator.NOTEQ, new StringLiteral("Invoice"));
 
-		}
-	}
+        public static final Predicate INVOICE_AND_CONFIDENTIAL = new Predicate(IS_CONFIDENTIAL, Operator.AND, IS_INVOICE);
+
+        @Override
+        public SQLQuery transform(Principal principal, SQLQuery query) {
+            NuxeoPrincipal nxPrinc = (NuxeoPrincipal) principal;
+
+            WhereClause where = query.where;
+            Predicate predicate;
+            Predicate predicate1;
+            Predicate predicate2;
+            if (!nxPrinc.isMemberOf("university")) {
+                if (where == null || where.predicate == null) {
+                    predicate1 = INVOICE_AND_CONFIDENTIAL;
+                    predicate2 = IS_NOT_INVOICE;
+                } else {
+                    // adds an ecm:primaryType = 'Invoice' AND university:confidential = true to the WHERE clause
+                    predicate1 = new Predicate(INVOICE_AND_CONFIDENTIAL, Operator.AND, where.predicate);
+                    predicate2 = new Predicate(IS_NOT_INVOICE, Operator.AND, where.predicate);
+
+                }
+                predicate = new Predicate(predicate1, Operator.OR, predicate2);
+                // return query with updated WHERE clause
+                return new SQLQuery(query.select, query.from, new WhereClause(predicate), query.groupBy, query.having,
+                        query.orderBy, query.limit, query.offset);
+            }
+            return query;
+
+        }
+    }
 }
